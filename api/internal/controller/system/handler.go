c09fc316ac98b305fc17d7b79317889bcc23d9ad
Ruf141 <mad0908141@gmail.com>
2025-01-23 21:11:05 +0900

    add comment

--- api/internal/controller/system/handler.go
+++ api/internal/controller/system/handler.go
@@ -4,6 +4,13 @@ import "github.com/gin-gonic/gin"
 
 type SystemHandlre struct{}
 
+// HealthCheck godoc
+// @Summary 死活監視用
+// @Tags healthcheck
+// @Accept json
+// @Produce json
+// @Success 200 {object} Response
+// @Router /v1/health [get]
 func (h *SystemHandlre) Helth(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{
 		"status": "ok",
