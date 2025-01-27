5e9908d3a4956241b6739f28fa5af2a0ac060ee4
Ruf141 <mad0908141@gmail.com>
2025-01-27 14:34:54 +0900

    add getUserById to handlaer.go

--- api/internal/controller/user/handler.go
+++ api/internal/controller/user/handler.go
@@ -8,9 +8,19 @@ type UserHandler struct {}
 // @Summary ユーザー一覧を取得
 // @Tags GetUsers
 // @Accept json
-// Produce json
-// Success 200 {object} []ResponseUser
-// Router /v1/users [get]
+// @Produce json
+// @Success 200 {object} []ResponseUser
+// @Router /v1/users [get]
 func(h *UserHandler) GetUsers(ctx *gin.Context){}
+
+// GetUseById godoc
+// @Summary ユーザー一覧を取得
+// @Tags GetUseById 
+// @Accept json
+// @Produce json
+// @Param request path string ture "ユーザーID"
+// @Success 200 {object} ResponseUser
+// @Router /v1/users/:id [get]
+func(h *UserHandler)GetUseById(ctx *gin.Context){}
 
 
